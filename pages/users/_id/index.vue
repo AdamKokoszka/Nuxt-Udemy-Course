a758bd69594b57622725e1cae6f77fe3da72b795
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-08-04 23:39:46 +0200

    Start subtitle Pages, Routing & Views

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,7 @@
+<template>
+ <div class="container">
+  <h1>A single user</h1>
+  <h3>ID: {{ $route.params.id }}</h3>
+<!--  http://localhost:3000/users/9-->
+ </div>
+</template>
---
+++ pages/users/_id/more-detail.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>More Details</h1>
+    <h3>ID: {{ $route.params.id }}</h3>
+<!--    http://localhost:3000/users/9/more-detail-->
+  </div>
+</template>
