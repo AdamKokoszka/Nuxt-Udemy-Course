d74374dcbdc59e305ea06eb0481c31fdae261a45
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-09-06 23:22:03 +0200

    Finish Pages, Routing & Views Section

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
+  <Header />
    <h1>Users layout</h1>
     <nuxt />
   </div>
 </template>
-
+<script>
+import Header from '@/components/Header.vue'
+  export default{
+    components: {
+      Header
+    }
+  }
+</script>
 <style>
   h1{
     color: #3d44ff;
