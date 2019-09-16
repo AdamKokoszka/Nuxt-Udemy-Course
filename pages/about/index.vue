2aa99fddf152008f61e428281ace6a6a6efb8def
AdamKokoszka <adam.kokoszka.it@gmail.com>
2019-09-16 14:22:01 +0200

    Adding External Fonts

---
+++ pages/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about-page">
+    <h1>This is About Me page!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati laboriosam fuga suscipit repellat, facere ut iusto libero repellendus, ad! Quasi.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati laboriosam fuga suscipit repellat, facere ut iusto libero repellendus, ad! Quasi.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati laboriosam fuga suscipit repellat, facere ut iusto libero repellendus, ad! Quasi.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati laboriosam fuga suscipit repellat, facere ut iusto libero repellendus, ad! Quasi.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati laboriosam fuga suscipit repellat, facere ut iusto libero repellendus, ad! Quasi.</p>
+  </div>
+</template>
+
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+
+</style>
--- pages/products/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Poducts</h1>
-    <nuxt-link to="/products/1">First produck</nuxt-link>
-    <nuxt-link to="/products/2">Second produck</nuxt-link>
-  </div>
-</template>
